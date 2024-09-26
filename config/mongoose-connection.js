aafe52b157fa1f2e6acec9773ee06a6b011826dd
Vivek ghoshi <the.vivekghoshi@gmail.com>
2024-09-26 12:01:01 +0530

    updated cart

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/CodeSoftProject")
+mongoose.connect("mongodb://127.0.0.1:27017/codeAlphaEcom")
 .then(function(){
     console.log('connected to mongoose')
 })
